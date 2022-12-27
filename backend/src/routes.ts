a801c5b29c1dd712943b890091dc9921e859b56c
Ezio Lemes <ti@ciclocairu.com.br>
2022-12-27 18:14:41 -0400

    add cadastro de produto

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,10 +4,12 @@ import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 
+import { isAuthenticated } from './middlewares/isAuthenticated';
+
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 import { ListCategoryController } from './controllers/category/ListCategoryController';
+import { CreateProductController } from './controllers/product/CreateProductController';
 
-import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const router = Router();
 
@@ -19,5 +21,8 @@ router.get('/me', isAuthenticated, new DetailUserController().handle);
 //-- ROTAS CATEGORY --
 router.post('/category', isAuthenticated, new CreateCategoryController().handle);
 router.get('/category', isAuthenticated, new ListCategoryController().handle);
+
+//-- ROTAS PRODUCT --
+router.post('/product', isAuthenticated, new CreateProductController().handle)
 
 export { router };
