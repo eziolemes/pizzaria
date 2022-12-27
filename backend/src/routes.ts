46ddae0f23e48512ab22f148c7443580f71df912
Ezio Lemes <ti@ciclocairu.com.br>
2022-12-27 17:27:06 -0400

    add cadastrar categoria

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,15 +4,18 @@ import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
+
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const router = Router();
 
 //-- ROTAS USER --
 router.post('/users', new CreateUserController().handle);
-
 router.post('/session', new AuthUserController().handle);
-
 router.get('/me', isAuthenticated, new DetailUserController().handle);
+
+//-- ROTAS CATEGORY --
+router.post('/category', isAuthenticated, new CreateCategoryController().handle);
 
 export { router };
