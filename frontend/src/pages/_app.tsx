baf1fe976222482ea2c000fa7c7a9b32b04a2b49
Ezio Lemes <ti@ciclocairu.com.br>
2022-12-30 09:59:54 -0400

    add contexto

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../../styles/globals.scss';
-import { AppProps } from "next/app"
+import { AppProps } from "next/app";
+import { AuthProvider } from '../contexts/AuthContext';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { useContext, FormEvent } from 'react';
+
 import Head from "next/head";
 import Image from "next/image";
 import styles from '../../styles/home.module.scss';
@@ -7,9 +9,23 @@ import logoImg from '../../public/logo.svg';
 import { Input } from "../components/ui/Input";
 import { Button } from "../components/ui/Button";
 
+import { AuthContext } from "../contexts/AuthContext";
+
 import Link from "next/link";
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext);
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+
+    let data = {
+      email: 'algum@teste.com',
+      password: "123123"
+    }
+    await signIn(data);
+  }
+
   return <>
     <Head>
       <title>SujeitoPizza - Faça seu login</title>
@@ -18,7 +34,7 @@ export default function Home() {
       <Image src={logoImg} alt="Logo sujeito pizzaria" />
 
       <div className={styles.login}>
-        <form action="">
+        <form onSubmit={handleLogin}>
           <Input 
             placeholder="Digite seu e-mail"
             type="text"
