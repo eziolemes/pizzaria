7166645402581deb3cbd154a4dd761371fa72b3b
Ezio Lemes <ti@ciclocairu.com.br>
2022-12-30 15:27:52 -0400

    add componente Header

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,10 +1,16 @@
 import { canSSRAuth } from "../../utils/canSSRAuth"
+import Head from "next/head";
+import { Header } from "../../components/Header";
 
 export default function Dashboard() {
   return(
-    <div>
-      <h1>Bem vindo ao painel</h1>
-    </div>
+    <>
+      <Head>
+        <title>Painel - Sujeito Pizzaria</title>
+      </Head>
+
+      <Header />
+    </>
   )
 }
 
