65974f294d7327d0ebf35d1e2d19e88fae32b4b7
Ezio Lemes <eziolemes@gmail.com>
2022-12-29 22:02:17 -0400

    criando tela login

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,33 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from '../../styles/home.module.scss';
+import { Input } from "../components/ui/Input";
+
+import logoImg from '../../public/logo.svg';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Sujeito pizza :)</h1>
-    </div>
+    <>
+      <Head>
+        <title>SujeitoPizza - Faça seu login</title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo sujeito pizzaria" />
+
+        <div className={styles.login}>
+          <form action="">
+            <Input 
+              placeholder="Digite seu e-mail"
+              type="text"
+            />
+
+            <Input 
+              placeholder="Digite sua senha"
+              type="password"
+            />
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
